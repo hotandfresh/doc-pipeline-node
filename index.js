532690f8c854262f4d259f50120c9fe509cb18b1
bomibear <bomibearly@gmail.com>
2019-10-01 11:16:27 -0700

    initial commit

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
-'use strict';
+"use strict";
 
-const {start} = require('./lib/server.js');
+// APP dependencies
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+
+// Global vars
+const PORT = process.env.PORT;
+
+// Make my server
+const app = express();
+app.use(cors());
+
+const { start } = require("./lib/server.js");
 
 start(process.env.PORT);
